5af8534f6a5a0e999a235fabba10a83f6205a630
unknown <>
2018-06-06 21:03:47 +0900

    ログイン追加

--- SSLand/SecondStreetAPI.cs
+++ SSLand/SecondStreetAPI.cs
@@ -12,6 +12,7 @@ namespace SSLand {
     public class SecondStreetAPI{
 
         private const string USER_AGENT = "Mozilla/5.0 (iPad; COU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89 Fril/6.7.1";
+        private const string LOGIN_USER_AGENT = "reuse_store_release/3.0.5 CFNetwork/897.15 Darwin/17.5.0";
         public const string agent = "reuse_store_release/3.0.2 CFNetwork/811.5.4 Darwin/16.7.0";
         private string proxy;
 
@@ -35,6 +36,49 @@ namespace SSLand {
 
         public SecondStreetAPI(Common.Account account){
             this.account = account;
+        }
+
+        public bool trySecondStreetLogin(string email, string password) {
+            string url = "https://auth.geonet.jp/authorize";
+            string client_id = "f92a4aeb4d2d07e4b8471177246e7eac3f7bf287ff984c2f3219c18c71b9c302";
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("response_type", "code");
+            param.Add("redirect_uri", "https://www.2ndstreet.jp/index.php/api_2_0/appuserauth/login");
+            param.Add("state", "state");
+            param.Add("scope", "openid profile delivery geoinfo geomasterid");
+            param.Add("client_id", client_id);
+            //csrf取り出し
+            var rawres = getSecondStreetAPI(url, param, SecondStreetAPI.LOGIN_USER_AGENT);
+            var resjson = DynamicJson.Parse(rawres.response);
+            string csrf_token = resjson.csrf_token;
+
+            Dictionary<string, string> param2 = new Dictionary<string, string>();
+            param2.Add("response_type", "code");
+            param2.Add("redirect_uri", "https://www.2ndstreet.jp/index.php/api_2_0/appuserauth/login");
+            param2.Add("client_id", client_id);
+            param2.Add("csrf_token", csrf_token);
+            param2.Add("user_action", "accept");
+            param2.Add("state", "state");
+            param2.Add("scope", "openid profile delivery geoinfo geomasterid");
+            param2.Add("login_id", email);
+            param2.Add("password", password);
+            //302で自動的にリダイレクトする
+            var rawres2 = postSecondStreetAPI(url, param2, SecondStreetAPI.LOGIN_USER_AGENT);
+            try {
+                var resjson2 = DynamicJson.Parse(rawres2.response);
+                string access_token = resjson2.access_token;
+                string refresh_token = resjson2.refresh_token;
+                /*string token_type = resjson.token_type;
+                int expires_in = (int)resjson.expires_in;
+                string geo_id = resjson.geo_id;
+                string nickname = resjson.nickname;
+                string geo_master_id = resjson.geo_master_id;*/
+                Log.Logger.Info("セカンドストリートログイン成功");
+                return true;
+            } catch (Exception ex) {
+                Log.Logger.Error("セカンドストリートログイン失敗");
+                return false;
+            }
         }
 
         //FIXIT:新着アイテムの取得
@@ -70,91 +114,101 @@ namespace SSLand {
             }
         }
 
-
-
-
-        //新着商品を取得する
-        //public List<SecondStreetListItem> getNewItems(int count = 30, string max_id = "")
-        //{
-        //    List<SecondStreetListItem> rst = new List<SecondStreetListItem>();
-        //    Dictionary<string, string> param = new Dictionary<string, string>();
-        //    param.Add("auth_token", this.account.fril_auth_token);
-        //    param.Add("count", count.ToString());
-        //    //if (string.IsNullOrEmpty(max_id) == false) param.Add("max_id", max_id);
-        //    string url = "http://api.fril.jp/api/v3/timelines/current";
-        //    SecondStreetRawResponse rawres = getFrilAPI(url, param);
-        //    if (rawres.error)
-        //    {
-        //        Log.Logger.Error(string.Format("フリルタイムライン取得失敗"));
-        //        return null;
-        //    }
-        //    dynamic resjson = DynamicJson.Parse(rawres.response);
-        //    try
-        //    {
-        //        foreach (var itemjson in resjson.items)
-        //        {
-        //            rst.Add(new SecondStreetListItem(itemjson));
-        //        }
-        //        Console.WriteLine("start: " + rst[0].item_id + " end: " + rst[rst.Count - 1].item_id);
-        //        return rst;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Log.Logger.Error("フリルタイムラインjsonパース失敗");
-        //        return rst;
-        //    }
-        //}
-
-
+        private SecondStreetRawResponse postSecondStreetAPI(string url, Dictionary<string, string> param, string UserAgent) {
+            SecondStreetRawResponse res = new SecondStreetRawResponse();
+            try {
+                string text = "";
+                List<string> paramstr = new List<string>();
+                int num = 0;
+                foreach (KeyValuePair<string, string> p in param) {
+                    string k = Uri.EscapeDataString(p.Key);
+                    string v = Uri.EscapeDataString(p.Value);
+                    if (num != 0) text += "&";
+                    text = text + (k + "=" + v);
+                    num++;
+                }
+                byte[] bytes = Encoding.ASCII.GetBytes(text);
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.UserAgent = UserAgent;
+                req.Method = "POST";
+                //リクエストヘッダを付加
+                req.ContentType = "application/x-www-form-urlencoded; charset=utf-8";
+                req.Accept = "gzip, deflate";//"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                //if (!string.IsNullOrEmpty(this.access_token)) req.Headers.Add("authorization", "OAuth2 " + this.access_token);
+                //req.Accept = "application/json";
+                req.ContentLength = (long)bytes.Length;
+                //クッキーコンテナの追加
+                req.CookieContainer = this.cc;
+                //タイムアウト設定
+                req.Timeout = 5000;
+                //POST
+                string content = "";
+                var task = Task.Factory.StartNew(() => executePostRequest(ref req, bytes));
+                task.Wait(10000);
+                if (task.IsCompleted)
+                    content = task.Result;
+                else
+                    throw new Exception("Timed out");
+                if (string.IsNullOrEmpty(content)) throw new Exception("webrequest error");
+                res.error = false;
+                res.response = content;
+                Log.Logger.Info("SecondStreetPOSTリクエスト成功");
+                req.Abort();
+                return res;
+            } catch (Exception e) {
+                return res;
+                Log.Logger.Error("SecondStreetPOSTリクエスト失敗");
+            }
+        }
 
         //FrilAPIをGETでたたく
-        //private SecondStreetRawResponse getFrilAPI(string url, Dictionary<string, string> param)
-        //{
-        //    SecondStreetRawResponse res = new SecondStreetRawResponse();
-        //    try
-        //    {
-        //        //url = Uri.EscapeUriString(url);//日本語などを％エンコードする
-        //        //パラメータをURLに付加 ?param1=val1&param2=val2...
-        //        url += "?";
-        //        List<string> paramstr = new List<string>();
-        //        foreach (KeyValuePair<string, string> p in param)
-        //        {
-        //            string k = Uri.EscapeDataString(p.Key);
-        //            string v = Uri.EscapeDataString(p.Value);
-        //            paramstr.Add(k + "=" + v);
-        //        }
-        //        url += string.Join("&", paramstr);
-        //        //HttpWebRequestの作成
-        //        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-        //        req.CookieContainer = this.cc;
-        //        req.UserAgent = SecondStreetAPI.USER_AGENT;
-        //        req.Method = "GET";
-        //        //プロキシの設定
-        //        if (string.IsNullOrEmpty(this.proxy) == false)
-        //        {
-        //            System.Net.WebProxy proxy = new System.Net.WebProxy(this.proxy);
-        //            req.Proxy = proxy;
-        //        }
-        //        //結果取得
-        //        string content = "";
-        //        var task = Task.Factory.StartNew(() => executeGetRequest(req));
-        //        task.Wait(10000);
-        //        if (task.IsCompleted)
-        //            content = task.Result;
-        //        else
-        //            throw new Exception("Timed out");
-        //        if (string.IsNullOrEmpty(content)) throw new Exception("webrequest error");
-        //        res.error = false;
-        //        res.response = content;
-        //        Log.Logger.Info("SecondStreetGETリクエスト成功");
-        //        return res;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Log.Logger.Error("SecondStreetGETリクエスト失敗");
-        //        return res;
-        //    }
-        //}
+        private SecondStreetRawResponse getSecondStreetAPI(string url, Dictionary<string, string> param, string UserAgent)
+        {
+            SecondStreetRawResponse res = new SecondStreetRawResponse();
+            try
+            {
+                //url = Uri.EscapeUriString(url);//日本語などを％エンコードする
+                //パラメータをURLに付加 ?param1=val1&param2=val2...
+                url += "?";
+                List<string> paramstr = new List<string>();
+                foreach (KeyValuePair<string, string> p in param)
+                {
+                    string k = Uri.EscapeDataString(p.Key);
+                    string v = Uri.EscapeDataString(p.Value);
+                    paramstr.Add(k + "=" + v);
+                }
+                url += string.Join("&", paramstr);
+                //HttpWebRequestの作成
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.CookieContainer = this.cc;
+                req.UserAgent = UserAgent;
+                req.Method = "GET";
+                //プロキシの設定
+                if (string.IsNullOrEmpty(this.proxy) == false)
+                {
+                    System.Net.WebProxy proxy = new System.Net.WebProxy(this.proxy);
+                    req.Proxy = proxy;
+                }
+                //結果取得
+                string content = "";
+                var task = Task.Factory.StartNew(() => executeGetRequest(req));
+                task.Wait(10000);
+                if (task.IsCompleted)
+                    content = task.Result;
+                else
+                    throw new Exception("Timed out");
+                if (string.IsNullOrEmpty(content)) throw new Exception("webrequest error");
+                res.error = false;
+                res.response = content;
+                Log.Logger.Info("SecondStreetGETリクエスト成功");
+                return res;
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error("SecondStreetGETリクエスト失敗");
+                return res;
+            }
+        }
         private string executeGetRequest(HttpWebRequest req)
         {
             try
@@ -170,7 +224,7 @@ namespace SSLand {
                 return "";
             }
         }
-        private string executePostRequest(HttpWebRequest req, byte[] bytes)
+        private string executePostRequest(ref HttpWebRequest req, byte[] bytes)
         {
             try
             {
@@ -188,78 +242,16 @@ namespace SSLand {
                     }
                 }
             }
-            catch
+            catch(WebException ex)
             {
+                using (var stream = ex.Response.GetResponseStream())
+                using (var reader = new StreamReader(stream)) {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
                 return "";
             }
         }
         private CookieContainer cc = new CookieContainer();
-        private string postMultipartFril(string url, Dictionary<string, string> param, string file) {
-            Encoding encoding = Encoding.GetEncoding("UTF-8");
-            string text = Environment.TickCount.ToString();
-            byte[] bytes = encoding.GetBytes("\r\n");
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.UserAgent = USER_AGENT;
-            httpWebRequest.CookieContainer = this.cc;
-            httpWebRequest.Method = "POST";
-            httpWebRequest.ContentType = "multipart/form-data; boundary=" + text;
-            string text2 = "";
-            foreach (KeyValuePair<string, string> current in param) {
-                text2 = string.Concat(new string[]
-				{   
-					text2,
-					"--",
-					text,
-					"\r\nContent-Disposition: form-data; name=\"",
-					current.Key,
-					"\"\r\n\r\n",
-					current.Value,
-					"\r\n"
-				});
-            }
-            long num = 0L;
-            Path.GetFileName(file);
-            string s = "--" + text + "\r\nContent-Disposition: form-data; name=\"image\"; filename=\"image.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
-            byte[] bytes2 = encoding.GetBytes(s);
-            num += (long)encoding.GetBytes(s).Length + new FileInfo(file).Length;
-            byte[] bytes3 = encoding.GetBytes(text2);
-            byte[] bytes4 = encoding.GetBytes("--" + text + "--\r\n");
-            httpWebRequest.ContentLength = (long)bytes3.Length + num + (long)bytes.Length + (long)bytes4.Length;
-            string result;
-            using (Stream requestStream = httpWebRequest.GetRequestStream()) {
-                requestStream.Write(bytes3, 0, bytes3.Length);
-                using (FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read)) {
-                    requestStream.Write(bytes2, 0, bytes2.Length);
-                    byte[] array = new byte[4096];
-                    while (true) {
-                        int num2 = fileStream.Read(array, 0, array.Length);
-                        if (num2 == 0) {
-                            break;
-                        }
-                        requestStream.Write(array, 0, num2);
-                    }
-                    requestStream.Write(bytes, 0, bytes.Length);
-                }
-                requestStream.Write(bytes4, 0, bytes4.Length);
-                WebResponse webResponse = null;
-                string text3 = "";
-                try {
-                    webResponse = httpWebRequest.GetResponse();
-                    Log.Logger.Info("access info:url->" + url);
-                } catch (WebException ex) {
-                    webResponse = (HttpWebResponse)ex.Response;
-                    Log.Logger.Info("access info:url->" + url + " message->" + ex.Message);
-                } finally {
-                    using (Stream responseStream = webResponse.GetResponseStream()) {
-                        using (StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("UTF-8"))) {
-                            text3 = streamReader.ReadToEnd();
-                        }
-                    }
-                }
-                result = text3;
-            }
-            return result;
-        }
 
         }
     }
