55030043e470ec9f1afc739d2bedba30063e86c3
unknown <>
2018-06-08 15:07:08 +0900

    アカウント管理完成

--- SSLand/Account.Designer.cs
+++ SSLand/Account.Designer.cs
@@ -31,74 +31,84 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.Size = new System.Drawing.Size(73, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "アカウント情報";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Location = new System.Drawing.Point(9, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 15);
+            this.label2.Size = new System.Drawing.Size(98, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "GEO ID(ログインID)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 110);
+            this.label3.Location = new System.Drawing.Point(9, 88);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.Size = new System.Drawing.Size(52, 12);
             this.label3.TabIndex = 2;
             this.label3.Text = "パスワード";
             // 
-            // textBox1
+            // emailTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 72);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(365, 22);
-            this.textBox1.TabIndex = 3;
+            this.emailTextBox.Location = new System.Drawing.Point(11, 58);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(275, 19);
+            this.emailTextBox.TabIndex = 3;
             // 
-            // textBox2
+            // passwordTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(15, 139);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(365, 22);
-            this.textBox2.TabIndex = 4;
+            this.passwordTextBox.Location = new System.Drawing.Point(11, 111);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(275, 19);
+            this.passwordTextBox.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(488, 139);
+            this.button1.Location = new System.Drawing.Point(330, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.Size = new System.Drawing.Size(102, 53);
             this.button1.TabIndex = 5;
-            this.button1.Text = "機能なし（現状）";
+            this.button1.Text = "ログイン";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Account
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(448, 146);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Account";
-            this.Text = "Account";
+            this.Text = "アカウント管理";
+            this.Load += new System.EventHandler(this.Account_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,8 +119,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.Button button1;
     }
 }
--- SSLand/Account.cs
+++ SSLand/Account.cs
@@ -16,5 +16,24 @@ namespace SSLand
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var api = new SecondStreetAPI();
+            bool loginres = api.trySecondStreetLogin(emailTextBox.Text.Trim(), passwordTextBox.Text.Trim());
+            if (loginres) {
+                var settingsDBHelper = new SettingsDBHelper();
+                settingsDBHelper.updateSettings("email", this.emailTextBox.Text.Trim());
+                settingsDBHelper.updateSettings("password", this.passwordTextBox.Text.Trim());
+                MessageBox.Show("ログインに成功しました", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("ログインに失敗しました", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Account_Load(object sender, EventArgs e) {
+            var settingsDBHelper = new SettingsDBHelper();
+            this.emailTextBox.Text = settingsDBHelper.getSettingValue("email");
+            this.passwordTextBox.Text = settingsDBHelper.getSettingValue("password");
+        }
     }
 }
