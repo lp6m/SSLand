ba3592be0447cea9e6b075c75718c4a86f9483b2
unknown <>
2018-06-08 15:15:24 +0900

    getSecondStreetAPIWithLogin追加

--- SSLand/Common.cs
+++ SSLand/Common.cs
@@ -55,50 +55,19 @@ namespace SSLand
                 return UNIX_EPOCH.ToLocalTime();
             }
         }
-        //static public SecondStreetAPI getFrilAPIFromDB()
-        //{
-        //    var settingsDBHelper = new SettingsDBHelper();
-        //    string email = settingsDBHelper.getSettingValue("email");
-        //    string password = settingsDBHelper.getSettingValue("password");
-        //    string expiration_date_str = settingsDBHelper.getSettingValue("expiration_date");
-        //    string auth_token = settingsDBHelper.getSettingValue("auth_token");
-        //    string nickname = settingsDBHelper.getSettingValue("nickname");
-        //    string userid = settingsDBHelper.getSettingValue("userid");
-        //    if (email == null || password == null || expiration_date_str == null || auth_token == null || nickname == null || userid == null) return null;
-        //    DateTime expiration_date = DateTime.Parse(expiration_date_str);
-        //    Account account = new Account(email, password, auth_token, nickname, userid, expiration_date);
-        //    return checkFrilAPI(new FrilAPI(account));
-        //}
+        //DBからemailとpass拾ってきてログインを行い,Cookie付きのAPIインスタンスを返す
+        //失敗:DBに情報ない or ログイン失敗:null返す
+        static public SecondStreetAPI getSecondStreetAPIWithLogin(){
+            var settingsDBHelper = new SettingsDBHelper();
+            string email = settingsDBHelper.getSettingValue("email");
+            string password = settingsDBHelper.getSettingValue("password");
+            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) return null;
+            var api = new SecondStreetAPI();
+            bool loginres = api.trySecondStreetLogin(email, password);
+            if(loginres) return api;
+            else return null;
+        }
 
-        //public static FrilAPI checkFrilAPI(FrilAPI api)
-        //{
-        //    //有効期限が切れていたら更新する
-        //    FrilAPI rst = api;
-        //    if (api.account.expirationDate < DateTime.Now)
-        //    {
-        //        Log.Logger.Info("トークン更新開始");
-        //        try
-        //        {
-        //            FrilAPI newapi = new FrilAPI(api.account.email, api.account.password);
-        //            bool loginres = newapi.tryFrilLogin();
-        //            if (!loginres) throw new Exception("fril login exception");
-        //            var settingsDBHelper = new SettingsDBHelper();
-        //            settingsDBHelper.updateSettings("auth_token", newapi.account.fril_auth_token);
-        //            settingsDBHelper.updateSettings("expiration_date", newapi.account.expirationDate.ToString());
-        //            Log.Logger.Info("トークン更新成功");
-        //            return newapi;
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Log.Logger.Error("トークン更新中エラー : " + ex.Message);
-        //            return api;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return api;
-        //    }
-        //}
         // Base64エンコード
         public static string Base64Encode(string str)
         {
